c1a221706e45bfc6234031d6f695c20e358c6d5b
George Hilios <ghilios@gmail.com>
2023-06-25 09:39:17 -0400

    Bump version

--- Joko.NINA.Plugins/Joko.NINA.Plugins.HocusFocus/Properties/AssemblyInfo.cs
+++ Joko.NINA.Plugins/Joko.NINA.Plugins.HocusFocus/Properties/AssemblyInfo.cs
@@ -18,8 +18,8 @@ using System.Runtime.InteropServices;
 
 // [MANDATORY] The assembly versioning
 //Should be incremented for each new release build of a plugin
-[assembly: AssemblyVersion("3.0.0.11")]
-[assembly: AssemblyFileVersion("3.0.0.11")]
+[assembly: AssemblyVersion("3.0.0.12")]
+[assembly: AssemblyFileVersion("3.0.0.12")]
 
 // [MANDATORY] The name of your plugin
 [assembly: AssemblyTitle("Hocus Focus")]
